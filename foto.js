fc74d42312212a62af60fffc73778f9b0e616b1c
Alfiana E. Sibuea <alfian.sibuea@gmail.com>
2014-09-18 19:39:29 +0700

    Use smaller DOMReady watcher

--- foto.js
+++ foto.js
@@ -12,121 +12,42 @@
     'use strict';
 
     // DOM Ready function
-    // https://github.com/addyosmani/jquery.parts/blob/master/jquery.documentReady.js
-    var ready = (function() {
+    // https://github.com/dperini/ContentLoaded/blob/master/src/contentloaded.js
+    function ready(win, fn) {
 
-        // Use the correct document accordingly with window argument (sandbox)
-        var document = window.document,
-            DOMFinish = false,
-            readyBound = false,
-            callbackQueue = [];
+        var done = false, top = true,
 
-        function registerOrRunCallback(callback) {
-            if (typeof callback === 'function') {
-                callbackQueue.push(callback);
+        doc = win.document,
+        root = doc.documentElement,
+        modern = doc.addEventListener,
+
+        add = modern ? 'addEventListener' : 'attachEvent',
+        rem = modern ? 'removeEventListener' : 'detachEvent',
+        pre = modern ? '' : 'on',
+
+        init = function(e) {
+            if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
+            (e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
+            if (!done && (done = true)) fn.call(win, e.type || e);
+        },
+
+        poll = function() {
+            try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+            init('poll');
+        };
+
+        if (doc.readyState == 'complete') fn.call(win, 'lazy');
+        else {
+            if (!modern && root.doScroll) {
+                try { top = !win.frameElement; } catch(e) { }
+                if (top) poll();
             }
+            doc[add](pre + 'DOMContentLoaded', init, false);
+            doc[add](pre + 'readystatechange', init, false);
+            win[add](pre + 'load', init, false);
         }
 
-        function DOMReadyCallback() {
-            while(callbackQueue.length) {
-                (callbackQueue.shift())();
-            }
-            registerOrRunCallback = function( callback ) {
-                callback();
-            };
-        }
-
-        // The ready event handler
-        function DOMContentLoaded() {
-            if (document.addEventListener) {
-                document.removeEventListener( "DOMContentLoaded", DOMContentLoaded, false );
-            } else {
-                // we're here because readyState !== "loading" in oldIE
-                // which is good enough for us to call the dom ready!
-                document.detachEvent( "onreadystatechange", DOMContentLoaded );
-            }
-            DOMReady();
-        }
-
-        // Handle when the DOM is ready
-        function DOMReady() {
-            // Make sure that the DOM is not already loaded
-            if ( !DOMFinish ) {
-                // Make sure body exists, at least, in case IE gets a little overzealous (ticket #5443).
-                if ( !document.body ) {
-                    return setTimeout( DOMReady, 1 );
-                }
-                // Remember that the DOM is ready
-                DOMFinish = true;
-                // If there are functions bound, to execute
-                DOMReadyCallback();
-                // Execute all of them
-            }
-        }
-
-        function bindReady() {
-            var toplevel = false;
-
-            if ( readyBound ) {
-                return;
-            }
-            readyBound = true;
-
-            // Catch cases where $ is called after the
-            // browser event has already occurred.
-            if ( document.readyState !== "loading" ) {
-                DOMReady();
-            }
-
-            // Mozilla, Opera and webkit nightlies currently support this event
-            if ( document.addEventListener ) {
-                // Use the handy event callback
-                document.addEventListener( "DOMContentLoaded", DOMContentLoaded, false );
-                // A fallback to window.onload, that will always work
-                window.addEventListener( "load", DOMContentLoaded, false );
-                // If IE event model is used
-            } else if ( document.attachEvent ) {
-                // ensure firing before onload,
-                // maybe late but safe also for iframes
-                document.attachEvent( "onreadystatechange", DOMContentLoaded );
-                // A fallback to window.onload, that will always work
-                window.attachEvent( "onload", DOMContentLoaded );
-                // If IE and not a frame
-                // continually check to see if the document is ready
-                try {
-                    toplevel = window.frameElement == null;
-                } catch (e) {}
-                if ( document.documentElement.doScroll && toplevel ) {
-                    doScrollCheck();
-                }
-            }
-        }
-
-        // The DOM ready check for Internet Explorer
-        function doScrollCheck() {
-            if (DOMFinish) {
-                return;
-            }
-            try {
-                // If IE is used, use the trick by Diego Perini
-                // http://javascript.nwbox.com/IEContentLoaded/
-                document.documentElement.doScroll("left");
-            } catch (error) {
-                setTimeout( doScrollCheck, 1 );
-                return;
-            }
-            // and execute any waiting functions
-            DOMReady();
-        }
-
-        function ready(callback) {
-            registerOrRunCallback(callback);
-            bindReady();
-        }
-
-        return ready;
-
-    })();
+    }
 
     /**
      * Extract ft-*-src attribute
@@ -226,7 +147,7 @@
     }
 
     // Start!
-    ready(function() { 
+    ready(w, function() { 
 
         init(); 
 
